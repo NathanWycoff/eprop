82c51493a1b038400e2051cbba0fa0a0b7666071
Nathan Wycoff <nathanbrwycoff@gmail.com>
2019-11-10 13:53:46 -0500

    Created random problem generator

--- python/bptt.py
+++ python/bptt.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#  python/playground.py Author "Nathan Wycoff <nathanbrwycoff@gmail.com>" Date 10.29.2019
+#  python/tf_playground.py Author "Nathan Wycoff <nathanbrwycoff@gmail.com>" Date 11.04.2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tqdm import tqdm
+exec(open("python/lib.py").read())
 
 np.random.seed(123)
 
@@ -15,15 +18,19 @@ T_EPS = 0.01
 T_STEPS = 1000
 T_END = T_EPS * T_STEPS
 THRESH = 1.0
+GAMMA = 0.3 #Smoothing constant on pseudo-gradient 
+LEARN_RATE = 1e-4
+EPOCHS = 100
 
 #TODO: Alpha should depend on T_EPS
 alpha = 0.99
 kappa = np.exp(-T_EPS)
 
 mult = 100. / np.sqrt(H)
-THETA_in = mult * np.random.normal(size=[H,P]) * T_EPS
-THETA_rec = mult * np.random.normal(size=[H,H]) * T_EPS
-THETA_out = np.random.normal(size=[Q,H])
+THETA_in = tf.Variable(mult * np.random.normal(size=[H,P]) * T_EPS)
+THETA_rec = tf.Variable(mult * np.random.normal(size=[H,H]) * T_EPS)
+THETA_out = tf.Variable(np.random.normal(size=[Q,H]))
+trainable_variables = [THETA_in, THETA_rec, THETA_out]
 
 # A list of lists. The inner lists contain integers, giving the index of any neuron that has spiked.
 spikes = [[] for _ in range(T_STEPS+1)]
@@ -40,37 +47,66 @@ for spike in spikes2:
 
 inlist = [spikes1, spikes2]
 
-Vs = np.zeros([H,T_STEPS+1])
+target = np.ones([T_STEPS]) - 2 * np.array(np.floor(np.linspace(0,T_STEPS*T_EPS, T_STEPS)) % 2 == 0).astype(np.float64)
+
+#Vs = np.zeros([H,T_STEPS+1])
+V_last = tf.Variable(np.zeros([H,1]), dtype = tf.float64)
+V = tf.Variable(np.zeros([H,1]), dtype = tf.float64)
+spikevec = tf.Variable(np.zeros([H,1]), dtype = tf.float64)
+spikevec_last = tf.Variable(np.zeros([H,1]), dtype = tf.float64)
 y = np.zeros([Q, T_STEPS+1])
 
-# Forward Simulation!
+@tf.custom_gradient
+def snl(V):
+    """
+    The threshold activation function typical in LIF models, but with a smoothed gradient.
+    """
+    def grad(dy):
+        return dy * GAMMA * tf.keras.activations.relu(1. - tf.abs(THRESH-V)) / THRESH
+    val = tf.cast(V >= THRESH, tf.float64)
+    return val, grad
 
-for t in range(T_STEPS):
-    # Decay Potential
-    Vs[:,t+1] = alpha * Vs[:,t]
+optim = tf.keras.optimizers.SGD(learning_rate = LEARN_RATE)
 
-    # Integrate incoming spikes
-    Vs[:,t+1] += np.sum(THETA_rec[:,spikes[t]], axis = 1)
+costs = np.empty([EPOCHS])
+for epoch in tqdm(range(EPOCHS)):
+    with tf.GradientTape() as gt:
+        cost = 0
+        for t in range(T_STEPS):
+            # Decay Potential
+            V = alpha * V_last
 
-    # Integrate external stimuli
-    Vs[:,t+1] += np.sum(THETA_in[:,in_spikes[t]], axis = 1)
+            # Integrate incoming spikes
+            V += tf.matmul(THETA_rec, spikevec_last)
 
-    # Add outgoing spikes to the stack and reset potentials as necessary.
-    spikevec = Vs[:,t+1] >= THRESH
-    spikes[t+1] = list(np.where(spikevec)[0])
-    Vs[:,t+1] *= (1-spikevec)
+            # Integrate external stimuli
+            inspikevec = np.zeros([P,1])
+            for i in in_spikes[t]:
+                inspikevec[i] = 1.
+            V += tf.matmul(THETA_in, inspikevec)
 
-    # Record network output
-    y[:,t+1] = np.sum(THETA_out[:,spikes[t]]) + kappa * y[:,t]
+            # Add outgoing spikes to the stack and reset potentials as necessary.
+            spikevec = snl(V)
+            spikes[t+1] = list(np.where(spikevec==1.)[0])
+            V *= (1-spikevec)
 
-# Get a gradient!
-    # For now, naively do it for every neuron.
-for h1 in range(H):
-    for h2 in range(H):
-        dtheta = 0
-        for t in range(T_STEPS, 0, -1):
+            # Calculate output
+            contrib = tf.matmul(THETA_out, spikevec)
+            yout = contrib + kappa * y[:,t]
+            y[:,t+1] = yout.numpy()
 
+            # Determine error
+            delta = yout - target[t]
+            cost += tf.square(delta)
 
+            # Update state
+            V_last = V
+            spikevec_last = spikevec
 
+    grad = gt.gradient(cost, trainable_variables)
+    grads_n_vars = [(grad[i], trainable_variables[i]) for i in range(len(grad))]
+    optim.apply_gradients(grads_n_vars)
 
-print(max([len(x) for x in spikes]))
+    costs[epoch] = cost.numpy()
+
+plot_res(y, inlist, "forward.pdf")
--- python/lib.py
+++ python/lib.py
@@ -20,3 +20,35 @@ def plot_res(y, inlist, path = "temp.pdf"):
 
     plt.savefig(path)
     plt.close()
+
+def gen_problem(t_steps, t_eps = 0.01, min_seq_len = 1.0, hz = 10):
+    """
+    Generate a random problem from a simple class. Randomly have one of two inputs switch on for some amount of time. One input corresponds to a target of 1, and the other a taret of -1.
+
+    T_EPS is the step size of our problem, and is assumed to be 0.01
+    T_STEPS is the number of steps. Simulation time = T_EPS * T_STEPS
+    min_seq_len is the length, in seconds, that has to ellapse before we allow the inputs to change.
+    hz The frequency, in Hertz, of the signal, to be matched approximately.
+    """
+
+    assert t_eps == 0.01
+    assert min_seq_len == 1.0
+    assert hz == 10
+
+    t_end = t_steps * t_eps
+
+    seqs = int(np.ceil(T_STEPS * T_EPS))
+    seqvals = np.random.choice(2,seqs)
+
+    inlist = [[], []]
+    for i,s in enumerate(seqvals):
+        inlist[s].extend(np.arange(float(i), i+1.0, step = 1./hz))
+
+    in_spikes = [[] for _ in range(T_STEPS+1)]
+    for i,spiketrain in enumerate(inlist):
+        for spike in spiketrain:
+            in_spikes[int(spike/t_eps)] = [i]
+
+    target = np.concatenate([((1-s) * -1. + s * 1.) * np.repeat(1.0, int(1/t_eps)) for s in seqvals])
+
+    return in_spikes, inlist, target
